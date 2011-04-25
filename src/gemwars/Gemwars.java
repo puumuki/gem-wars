8f5fc3a83b7a7c807d50f3c006217f655bf9a976
yaamboo@gmail.com <yaamboo@gmail.com@ca86eba7-ffd2-b4d5-8079-a3ff1f17e894>
2011-04-25 18:29:08 +0000

    jee, eka kommitti

---
+++ src/gemwars/Gemwars.java
@@ -0,0 +1,19 @@
+package gemwars;
+
+/**
+ * Tästä se lähtee.
+ * @author Yaamboo
+ *
+ */
+public class Gemwars {
+
+	/**
+	 * Pääohjelma
+	 * @param args ei käytössä
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world! Tästä tulloo Gemwarssia.");
+
+	}
+
+}
