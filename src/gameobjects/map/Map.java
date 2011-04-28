edb2e6230c5258dc18e90c61ef4cfbf03e1e34ef
yaamboo@gmail.com <yaamboo@gmail.com@ca86eba7-ffd2-b4d5-8079-a3ff1f17e894>
2011-04-28 07:07:29 +0000

    some map reading

--- src/gameobjects/map/Map.java
+++ src/gameobjects/map/Map.java
@@ -1,10 +1,13 @@
 package gameobjects.map;
 
+import java.io.InputStream;
 import java.util.List;
+import java.util.Scanner;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.util.ResourceLoader;
 
 import gameobjects.AEntity;
 
@@ -15,7 +18,57 @@ public class Map extends AEntity {
 	private static final int MAX_WIDTH = 1024;
 	private static final int MAX_HEIGHT = 1024;
 	
-	public void loadMap() {
+	private String name;
+	private String creator;
+	private int gemCount;
+	private int time;
+	
+	public void loadMap(String mapName) throws SlickException {
+		Scanner f = null;
+		
+		try {
+			InputStream in = ResourceLoader.getResourceAsStream("resources/maps/" + mapName);
+			f = new Scanner(in);
+			
+			while (f.hasNextLine()) {
+				// read the file one line at a time
+				
+				// first the map name
+				name = f.nextLine();
+				
+				// creator name
+				creator = f.nextLine();
+				
+				// gem count
+				int gems = 0;
+				try {
+					gems = Integer.parseInt(f.nextLine());
+				} catch (NumberFormatException e) {
+					gems = 0;
+				}
+				gemCount = gems;
+				
+				// time
+				int time = 60;
+				try {
+					time = Integer.parseInt(f.nextLine());
+				} catch (NumberFormatException e) {
+					time = 60;
+				}
+				this.time = time;
+				
+				// TODO: Then layers, one at a time
+				// also: what to do, if map is incomplete? 
+			}
+			
+			
+		} catch (RuntimeException e) {
+			throw new SlickException("Error loading map file", e); 
+		}
+		finally {
+			if (f != null)
+				f.close();
+		}
 		
 	}
 	
