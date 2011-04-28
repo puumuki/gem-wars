7c38be05d6bf39b4d9218ffc107518613de8dcb0
yaamboo@gmail.com <yaamboo@gmail.com@ca86eba7-ffd2-b4d5-8079-a3ff1f17e894>
2011-04-28 19:49:03 +0000

    Nice looking version :)

--- src/gemwars/Gemwars.java
+++ src/gemwars/Gemwars.java
@@ -53,7 +53,7 @@ public class Gemwars extends StateBasedGame {
 	public static void main(String[] args) throws SlickException {		
         AppGameContainer app = new AppGameContainer(new Gemwars());
         
-        app.setDisplayMode(640, 480, false);
+        app.setDisplayMode(1600, 1000, false);
         app.start();
 	}
 	
