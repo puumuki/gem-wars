d9081fd8cbe73528de76addb0b0d7222caa4d6b3
Teemuki <Teemuki@ca86eba7-ffd2-b4d5-8079-a3ff1f17e894>
2011-04-30 08:32:21 +0000

    Changed Property annotation Target to fields only Improved TestOptions

--- src/test/TestOptions.java
+++ src/test/TestOptions.java
@@ -3,27 +3,61 @@ package test;
 import io.Options;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Properties;
 
 import junit.framework.Assert;
 
 
+import org.junit.After;
 import org.junit.Test;
 
 public class TestOptions {
 	
+	@After
+	public void cleanUp() {
+		try {
+			File file = new File("testGemwars.properties");
+			
+			if( file.exists() ) {
+				file.delete();						
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
 	
 	@Test
-	public void testLoadingOptions() throws IllegalArgumentException, IllegalAccessException, IOException {
+	public void testLoadingOptions() throws IllegalArgumentException, IllegalAccessException, IOException {			
+		
 		
 		Options options = Options.getInstance();
 		options.setMusicVolume(3f);
 		options.setSoundVolume(2.1f);		
-		options.save( new File("Gemwars.properties"));		
-		options.load(new File("Gemwars.properties"));
+		
+		
+		options.save( new File("testGemwars.properties"));
+		
+		Properties properties = new Properties();
+		
+		properties.load( new FileInputStream("testGemwars.properties"));
+		
+		properties.setProperty("screenwidth", "999");
+		properties.setProperty("screenheight", "888");
+		FileOutputStream stream = new FileOutputStream(new File("testGemwars.properties"));
+		properties.store(stream, "");
+		
+		options.load(new File("testGemwars.properties"));
 		
 		Assert.assertEquals(3f, options.getMusicVolume());
 		Assert.assertEquals(3f, options.getMusicVolume());
+		
+		Assert.assertEquals( new Integer(999), options.getScreenWitdh());
+		Assert.assertEquals( new Integer(888), options.getScreenHeight());
+		
+		stream.close();
 	}
 }
